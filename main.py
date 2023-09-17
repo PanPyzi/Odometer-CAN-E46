ca4199edacd112273bb9f8d4b458f91d71e7de1f
PanPyzi <thekolcioplaygames@gmail.com>
2023-09-17 23:37:43 +0200

    Basis down

--- main.py
+++ main.py
@@ -1,64 +1,78 @@
+from socketcan import CanRawSocket, CanFrame
+import curses
 
 #Init important vars
-RPM_D=0
-WATER_T=-99
-CHECK_ENG=False
 
-def read():
-    file = open("txt/can.txt",'r')
-    line = file.readline()
-    codes={}
-    while line != '':
-        match line[21:24]:
-            case '338':#SMG???
-                pass
-            case '316': #RPM_DASHBOARD (Byte3&Byte2)
-                RPM_D=int(int(line[41:43]+line[38:40],16)/6.4)
-                print('RPM: '+str(RPM_D))
-            case '545':
-                B0=bin(int(line[32:34], 16))[2:]
-                #print(bin(int(line[41:43], 16)))
-                B3 = bin(int(line[41:43], 16))[2:]
-                if B0=='01':
-                    CHECK_ENG=True
-                if B3=='0010':
-                    OIL_LVL=True
-                if B3=='0100':
-                    OIL_PRESS=True
+stdscr= curses.initscr()
+curses.noecho()
+curses.cbreak()
+curses.curs_set(False)
 
-                #print(line)
+interface = "can0"
+socket = CanRawSocket(interface=interface)
 
-            case '613':
-                SPEED_S=int(line[35:37], 16)
-                print("SPEEEEEEEEEEEEEEEEEEEEEEEEED: "+ str(SPEED_S))
-                print(line)
-                print(line[35:37])
-            case '615':
-                pass
-            case '1f0':
-                pass
-            case '1f3':
-                pass
-            case '153':#SPEED
-                SPEED_D=line
-                # if RPM_D > 1100:
-                #     print(SPEED_D)
-                #print(line)
-            case '610':
-                pass
-            case '1f5':
-                pass
-            case '329':#WATER_TEMP (Byte1)
-                WATER_T=float((0.75*int(line[35:37],16)))-48.373
-                print("Water: "+str(WATER_T))
-                pass
+
+def read(w):
+    RPM_D = 0
+    WATER_T = -99
+    CHECK_ENG = False
+    ssss = ""
+    while True:
+        file = open("can_drive.txt", 'a')
+        frame = socket.recv()
+        can_data_B=frame.data
+        can_data = []
+        for i in range(0, len(frame.data.hex()))[::2]:
+            can_data.append(frame.data.hex()[i] + frame.data.hex()[i + 1])
+        can_id= str(hex(frame.can_id))
+        if can_id == '0x338':
+            pass
+        elif can_id == '0x316': #RPM_DASHBOARD (Byte3&Byte2)
+            RPM_D=int(int(str(can_data[3])+str(can_data[2]),16)/6.4)
+        elif can_id == '0x545':
+            # B0=bin(str(can_data[0]))
+            # #print(bin(int(line[41:43], 16)))
+            # B3 = bin(str(can_data[3]))
+            # if B0=='01':
+            #     CHECK_ENG=True
+            # if B3=='0010':
+            #     OIL_LVL=True
+            # if B3=='0100':
+            #     OIL_PRESS=True
+
+        elif can_id == '0x613':
+            SPEED_S=int(can_data[1], 16)*10
+            #file.write("can0  {0:8X}   [{1}]  {2} \n".format(frame.can_id, len(frame.data)," ".join(["{0:02X}".format(b) for b in frame.data])))
+            pass
+        elif can_id == '0x615':
+            pass
+        elif can_id == '0x1f0':
+            #file.write("can0  {0:8X}   [{1}]  {2} \n".format(frame.can_id, len(frame.data)," ".join(["{0:02X}".format(b) for b in frame.data])))
+            pass
+        elif can_id == '0x1f3':
+            pass
+        elif can_id == '0x153':#SPEED
+            file.write("can0  {0:8X}   [{1}]  {2} \n".format(frame.can_id, len(frame.data)," ".join(["{0:02X}".format(b) for b in frame.data])))
+            pass
+        elif can_id == '0x610':
+            #file.write("can0  {0:8X}   [{1}]  {2} \n".format(frame.can_id, len(frame.data)," ".join(["{0:02X}".format(b) for b in frame.data])))
+            pass
+        elif can_id == '0x1f5':
+            pass
+        elif can_id == '0x329':#WATER_TEMP (Byte1)
+            WATER_T=float((0.75*int(can_data[1],16)))-48.373
+
+        w.addstr(0, 1, "RPM: " + str(SPEED_S))
+        w.addstr(2,1,"RPM: "+str(RPM_D))
+        w.addstr(3,1,"Water: "+str(WATER_T))
+        w.addstr(4,1,"Check Engine:"+str(CHECK_ENG))
+        w.refresh()
+        file.close()
         # if(line[21:25] in codes):
         #     codes[line[21:25]]+=1
         # else:
         #     codes[line[21:25]]=1
         #print(line[21:24]+" "+line[30:55])
-        line  = file.readline()
 
-    print(codes)
 
-read()
+curses.wrapper(read)
